6c554e48657c291eff14000d8e422c420350223c
Victoria Avalos <inessavalos14@gmail.com>
2023-07-05 17:07:53 -0300

    Configurando rutas,vistas y estilos de Astro/Psico

--- Explorersci/urls.py
+++ Explorersci/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import Divulgacion
+from .views import Divulgacion, Psicologia,Astronomia
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Divulgacion.as_view(), name="Principal")
+    path('Divulgacion/', Divulgacion.as_view(), name="Principal"),
+    path('Psicologia/', Psicologia.as_view(), name="psicologia"),
+    path('Astronomia/', Astronomia.as_view(), name= "Astronomia")
 ]
--- Explorersci/views.py
+++ Explorersci/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class Divulgacion(TemplateView):
     template_name = "index.html"
+
+class Psicologia(TemplateView):
+    template_name = "psicologia.html"
+
+class Astronomia(TemplateView):
+    template_name = "astronomia.html"
