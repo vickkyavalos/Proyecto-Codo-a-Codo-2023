a26e0e3a31b8b967c7cc6b0f14367d2ea9758f3f
Jonathan Romero Benitez <jonisrb@hotmail.com.ar>
2023-07-06 16:35:16 -0300

    Creacion e integracion de views y urls; Conexion entre urls

--- Explorersci/urls.py
+++ Explorersci/urls.py
@@ -2,9 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import Divulgacion
+from .views import Divulgacion, Psicologia,Astronomia,Biologia,Nosotros,Formulario
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Divulgacion.as_view(), name="Principal")
+    path('', Divulgacion.as_view(), name="Principal"),
+    path('Psicologia/', Psicologia.as_view(), name="Psicologia"),
+    path('Astronomia/', Astronomia.as_view(), name= "Astronomia"),
+    path('Biologia/', Biologia.as_view(), name= "Biologia"),
+    path('Formulario/', Formulario.as_view(), name= "Formulario"),
+    path('Nosotros/', Nosotros.as_view(), name= "Nosotros")
 ]
--- Explorersci/views.py
+++ Explorersci/views.py
@@ -2,3 +2,18 @@ from django.views.generic import TemplateView
 
 class Divulgacion(TemplateView):
     template_name = "index.html"
+
+class Psicologia(TemplateView):
+    template_name = "psicologia.html"
+
+class Astronomia(TemplateView):
+    template_name = "astronomia.html"
+    
+class Biologia(TemplateView):
+    template_name = "bio4.html"
+    
+class Formulario(TemplateView):
+    template_name = "formulario.html"
+
+class Nosotros(TemplateView):
+    template_name = "nosotro.html"
