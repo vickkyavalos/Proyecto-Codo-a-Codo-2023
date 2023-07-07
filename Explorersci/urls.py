d933aab9050c258af907e26cbf0e57ae0c947d20
Jonathan Romero Benitez <jonisrb@hotmail.com.ar>
2023-07-07 01:32:46 -0300

    Integracion de templates, urls y view

--- Explorersci/urls.py
+++ Explorersci/urls.py
@@ -2,15 +2,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import Divulgacion, Psicologia,Astronomia,Biologia,Nosotros,Formulario
+from .views import Divulgacion, Astronomia,Biologia,Historia,Nosotros,Psicologia,Formulario,Contacto
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Divulgacion.as_view(), name="Principal"),
-    path('Psicologia/', Psicologia.as_view(), name="Psicologia"),
     path('Astronomia/', Astronomia.as_view(), name= "Astronomia"),
     path('Biologia/', Biologia.as_view(), name= "Biologia"),
+    path('Historia/', Historia.as_view(), name= "Historia"),
+    path('Psicologia/', Psicologia.as_view(), name="Psicologia"),
     path('Formulario/', Formulario.as_view(), name= "Formulario"),
+    path('Contacto/', Contacto.as_view(), name= "Contacto"),
     path('Nosotros/', Nosotros.as_view(), name= "Nosotros")
 ]
--- Explorersci/views.py
+++ Explorersci/views.py
@@ -3,17 +3,24 @@ from django.views.generic import TemplateView
 class Divulgacion(TemplateView):
     template_name = "index.html"
 
-class Psicologia(TemplateView):
-    template_name = "psicologia.html"
-
 class Astronomia(TemplateView):
     template_name = "astronomia.html"
-    
+
 class Biologia(TemplateView):
     template_name = "bio4.html"
-    
+
+class Historia(TemplateView):
+    template_name= "historia.html"
+
+class Psicologia(TemplateView):
+    template_name = "psicologia.html"
+        
 class Formulario(TemplateView):
     template_name = "formulario.html"
 
+class Contacto(TemplateView):
+    template_name = "contacto.html"
+
 class Nosotros(TemplateView):
     template_name = "nosotro.html"
+
